1ab06b5b2bc8b3be5ec9c658386ba950ea529724
Matt <40653288+vmetric@users.noreply.github.com>
2018-11-19 10:41:43 -0700

    Dynamic visibility and window size
    The visibility of the appropriate controls now changes based on what the selected material is ("Mix" displays additional controls, anything else hides them). The MainWindow width and height changes to accomodate the change in control visibility.

--- Group 4 Project Num2/MainWindow.xaml.cs
+++ Group 4 Project Num2/MainWindow.xaml.cs
@@ -25,5 +25,54 @@ namespace Group_4_Project_Num2
         {
             InitializeComponent();
         }
+
+        public void changeVisibility(Control control, bool visible)
+        {
+            if (visible)
+            {
+                control.Visibility = Visibility.Visible;
+            } else
+            {
+                control.Visibility = Visibility.Hidden;
+            }
+        }
+
+        string articleOfClothing = "";
+        string material1 = "";
+        string material2 = "";
+        int material1Percentage = 0;
+        int material2Percentage = 0;
+        double areaOfArticle = 0.0;
+        double timeToIgnite = 0.0;
+
+        private void ArticleDropDownClosed(object sender, EventArgs e)
+        {
+            MessageBox.Show(articleComboBox.SelectionBoxItem.ToString());
+
+        }
+
+        private void MaterialDropDownClosed(object sender, EventArgs e)
+        {
+            if (materialComboBox.SelectionBoxItem.ToString() == "Mix")
+            {
+                changeVisibility(label_FirstMaterial, true);
+                changeVisibility(label_SecondMaterial, true);
+                changeVisibility(comboBox_FirstMaterial, true);
+                changeVisibility(comboBox_SecondMaterial, true);
+                changeVisibility(label_MaterialTwoPercent, true);
+                changeVisibility(textBox_MaterialTwoPercent, true);
+                Application.Current.MainWindow.Width = 800;
+                Application.Current.MainWindow.Height = 250;
+            }
+            else
+            {
+                changeVisibility(label_FirstMaterial, false);
+                changeVisibility(label_SecondMaterial, false);
+                changeVisibility(comboBox_FirstMaterial, false);
+                changeVisibility(comboBox_SecondMaterial, false);
+                changeVisibility(label_MaterialTwoPercent, false);
+                changeVisibility(textBox_MaterialTwoPercent, false);
+            }
+        }
     }
 }
