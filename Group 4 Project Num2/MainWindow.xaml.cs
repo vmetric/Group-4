8e3268a54c3f3bf13acb76b66a1625dbaf3433b4
Matt <40653288+vmetric@users.noreply.github.com>
2018-11-22 20:02:11 -0700

    Create List<class> for material data
    Created a class to hold data points for the different materials. Then made a list of that class with data points in it.

--- Group 4 Project Num2/MainWindow.xaml.cs
+++ Group 4 Project Num2/MainWindow.xaml.cs
@@ -20,13 +20,47 @@ namespace Group_4_Project_Num2
     /// When given material and percentage of that material and article of clothing, program will
     /// estimate the time until ignition and other various burn percents.
     /// </summary>
+    /// 
+    public class Record
+    {
+        //public string material { get; set; }
+        //public double timeToIgnite { get; set; }
+        //public double timeTo3CM { get; set; }
+        //public double timeTo6CM { get; set; }
+        //public double timeTo9CM { get; set; }
+        //public double timeTo12CM { get; set; }
+        //public double timeTo15CM { get; set; }
+        //public double flameout { get; set; }
+        //public double burnDuration { get; set; }
+        public string material;
+        public double timeToIgnite;
+        public double timeTo3CM;
+        public double timeTo6CM;
+        public double timeTo9CM;
+        public double timeTo12CM;
+        public double timeTo15CM;
+        public double flameout;
+        public double burnDuration;
+
+        public Record(string material, double timeToIgnite, double timeTo3CM, double timeTo6CM, double timeTo9CM, double timeTo12CM, double timeTo15CM, double flameout, double burnDuration)
+        {
+            this.material = material;
+            this.timeToIgnite = timeToIgnite;
+            this.timeTo3CM = timeTo3CM;
+            this.timeTo6CM = timeTo6CM;
+            this.timeTo9CM = timeTo9CM;
+            this.timeTo12CM = timeTo12CM;
+            this.timeTo15CM = timeTo15CM;
+            this.flameout = flameout;
+            this.burnDuration = burnDuration;
+        }
+    }
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
-
         public void changeVisibility(Control control, bool visible)
         {
             if (visible)
@@ -45,6 +79,13 @@ namespace Group_4_Project_Num2
         int material2Percentage = -1;
         double areaOfArticle = -1.0;
         double timeToIgnite = -1.0;
+        List<Record> ledger = new List<Record> {
+            new Record("Cotton", 1.50, 5.20, 6.76, 7.70, 8.75, 9.57, 15.04, 13.54),
+            new Record("Wool", 16.84, 19.67, 21.12, 22.31, 23.56, 26.26, 33.63, 16.79),
+            new Record("Spandex", 0.80, 5.37, 7.27, 9.17, 10.66, 11.67, 17.34, 16.54),
+            new Record("Polyester", 1.50, 5.20, 6.76, 7.70, 8.75, 9.57, 15.04, 13.54),
+            new Record("Nomex", 1.63, 6.29, 4.19, 9.67, 7.09, 8.75, 13.774, 60),
+        };
 
         private void ArticleDropDownClosed(object sender, EventArgs e)
         {
